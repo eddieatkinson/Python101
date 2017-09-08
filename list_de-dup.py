c3c856b895845f2bb216d25d4841f4ee21e83b16
Eddie Atkinson <eddiebatkinson@gmail.com>
2017-09-08 09:44:11 -0400

    List practice

---
+++ list_de-dup.py
@@ -0,0 +1,11 @@
+in_list = [1, 3, 4, 3]
+dup_list = in_list[0:]
+dup_list2 = in_list[0:]
+# print dup_list
+# new_list = []
+for i in range (0, len(in_list)):
+	for j in range (0, len(in_list)):
+		if (j < i and in_list[i] == dup_list[j]):
+			dup_list2.pop(i)
+print dup_list2
+
