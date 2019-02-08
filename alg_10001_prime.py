0bdf0c86610cff0cccf4403aad7b399b82593f39
Eddie Atkinson <eddiebatkinson@gmail.com>
2019-02-08 14:31:16 -0500

    found the nth prime number

--- alg_10001_prime.py
+++ alg_10001_prime.py
@@ -1,23 +1,46 @@
-known_primes = []
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 
-def is_prime(n):
-	total_primes = len(known_primes)
-	for i in range (0, total_primes):
-		if (n % known_primes[i] == 0):
-			# Not prime!
-			return False
-		else:
-			continue
-	known_primes.append(n)
-	return True
+# What is the 10 001st prime number?
 
-def nth_prime(n):
-	for i in range (2, n * 15):
-		if len(known_primes) < n:
-			is_prime(i)
-		else:
-			break
-	print max(known_primes)
+# known_primes = []
 
-nth = int(raw_input("Which prime would you like? "))
-nth_prime(nth)
+# def is_prime(n):
+# 	total_primes = len(known_primes)
+# 	for i in range (0, total_primes):
+# 		if (n % known_primes[i] == 0):
+# 			# Not prime!
+# 			return False
+# 		else:
+# 			continue
+# 	known_primes.append(n)
+# 	return True
+
+# def nth_prime(n):
+# 	for i in range (2, n * 15):
+# 		if len(known_primes) < n:
+# 			is_prime(i)
+# 		else:
+# 			break
+# 	print max(known_primes)
+
+# nth = int(raw_input("Which prime would you like? "))
+# nth_prime(nth)
+
+known_primes = [2]
+# number_to_test = 3
+
+def is_prime(primes_list, n):
+  for prime in primes_list:
+    if n % prime == 0:
+      return False
+  return True
+
+def determin_nth_prime(n, number_to_test):
+  while len(known_primes) < (n + 1):
+    if is_prime(known_primes, number_to_test):
+      known_primes.append(number_to_test)
+    number_to_test += 1
+  print(known_primes[n - 1])
+
+# print(known_primes)
+determin_nth_prime(10001, 3)
