60b2612a7a1c1abc1aec249fc8c177476798bd43
Eddie Atkinson <eddiebatkinson@gmail.com>
2019-04-11 18:27:06 -0400

    simplified finding n numbers from end

--- self_powers.py
+++ self_powers.py
@@ -8,6 +8,5 @@ number_to_test = int(input('Number to test? '))
 numbers_from_end = int(input('How many from the end? '))
 power_sum = find_power_of_number_n_plus_all_lesser_powers(number_to_test)
 power_sum_as_string = str(power_sum)
-length_of_string = len(power_sum_as_string)
-last_n_digits = power_sum_as_string[length_of_string - numbers_from_end:]
+last_n_digits = power_sum_as_string[-numbers_from_end:]
 print(last_n_digits)
