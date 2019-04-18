e2589aba1e316066ec2da40d346b09de6aba724f
Eddie Atkinson <eddiebatkinson@gmail.com>
2019-04-17 21:37:59 -0400

    Started problem

---
+++ number_letter_counts.py
@@ -0,0 +1,38 @@
+one = 'one'
+two = 'two'
+three = 'three'
+four = 'four'
+five = 'five'
+six = 'six'
+seven = 'seven'
+eight = 'eight'
+nine = 'nine'
+ten = 'ten'
+eleven = 'eleven'
+twelve = 'twelve'
+thirteen = 'thirteen'
+fourteen = 'fourteen'
+fifteen = 'fifteen'
+sixteen = 'sixteen'
+seventeen = 'seventeen'
+eighteen = 'eighteen'
+nineteen = 'nineteen'
+twenty = 'twenty'
+thirty = 'thirty'
+forty = 'forty'
+fifty = 'fifty'
+sixty = 'sixty'
+seventy = 'seventy'
+eighty = 'eighty'
+ninety = 'ninety'
+hundred = 'hundred'
+thousand = 'thousand'
+ampersand = 'and'
+
+one_through_nineteen = [one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen, fourteen, fifteen, sixteen, seventeen, eighteen, nineteen]
+tens_place = [twenty, thirty, forty, fifty, sixty, seventy, eighty, ninety]
+
+def get_one_through_ninety_nine():
+  return 'We will work on this later'
+
+print(get_one_through_ninety_nine())
