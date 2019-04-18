8a26c7710a68ab1de64650954420a904e3b0a384
Eddie Atkinson <eddiebatkinson@gmail.com>
2019-04-18 15:39:52 -0400

    all letters in one through 1000 added together

--- number_letter_counts.py
+++ number_letter_counts.py
@@ -29,10 +29,28 @@ hundred = 'hundred'
 thousand = 'thousand'
 ampersand = 'and'
 
-one_through_nineteen = [one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen, fourteen, fifteen, sixteen, seventeen, eighteen, nineteen]
-tens_place = [twenty, thirty, forty, fifty, sixty, seventy, eighty, ninety]
+one_through_nineteen = ['', one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen, fourteen, fifteen, sixteen, seventeen, eighteen, nineteen]
+tens_place = ['', twenty, thirty, forty, fifty, sixty, seventy, eighty, ninety]
+
+# total_numbers = ''
 
 def get_one_through_ninety_nine():
-  return 'We will work on this later'
+  total_numbers = ''
+  for tens in tens_place:
+    if tens == '':
+      for num in one_through_nineteen:
+        total_numbers += tens + num
+    else:
+      for i in range(10):
+        total_numbers += tens + one_through_nineteen[i]
+  return total_numbers
 
-print(get_one_through_ninety_nine())
+def get_one_through_one_thousand():
+  total_numbers = 10 * get_one_through_ninety_nine()
+  total_numbers += (99 * 9) * ampersand
+  for i in range(1, 10):
+    total_numbers += 100 * (one_through_nineteen[i] + hundred)
+  total_numbers += one + thousand
+  return total_numbers
+
+print(len(get_one_through_one_thousand()))
