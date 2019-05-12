b1c5bcfcd5a69e4893177aa7d487d2f5c457730c
Eddie Atkinson <eddiebatkinson@gmail.com>
2019-05-12 11:25:14 -0400

    removed commented section

--- powerful_digit_counts.py
+++ powerful_digit_counts.py
@@ -4,8 +4,6 @@ def number_of_digits_equals_power(n, power):
     return True
   return False
 
-# def get_number_of_n_digit_nums_of_nth_power(n, power):
-
 number_that_qualify = 0
 for base in range(1, 10):
   for power in range(1, 300):
